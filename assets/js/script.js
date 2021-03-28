9c94a70e472c0fbef9b2ebe35634f27c0c047095
Shawn Paulsen <shawnpaulsen@Shawns-MacBook-Pro.local>
2021-03-27 20:01:06 -0600

    JS work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,59 @@
 
 var searchBtn = document.getElementById('get-weather');
-searchBtn.addEventListener('click', getApi);
+//searchBtn.addEventListener('click', getApi);
 
+
+var repoList = document.querySelector('ul');
+var fetchButton = document.getElementById('fetch-button');
 
 function getApi(event) {
   event.preventDefault();
-  //fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=denver&units=imperial&appid=820c7f8138019a10c7af3d05720e11d9';
+  // stops default click   {cityname} 'https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=7b56d24fc2a9f800b5f359ee3bc2a2f5';
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather';
   // using frequent console.log() to check function
   console.log('click');
 
+  fetch(requestUrl)
+   .then(function (response) {
+     return response.json();
+   })
+   .then(function (data) {
+     console.log(data)
+  });
+}  
+
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       for (var i = 0; i < data.length; i++) {
+//         var listItem = document.createElement('li');
+//         listItem.textContent = data[i].html_url;
+//         repoList.appendChild(listItem);
+//       }
+//     });
+// }
+
+// fetchButton.addEventListener('click', getApi);
+
   //grab user input
- var citySearch = document.getElementById('city-search').value;
+ //var citySearch = document.getElementById('city-search').value;
+ 
+
+ 
+//  ?q={city name}&appid=7b56d24fc2a9f800b5f359ee3bc2a2f5
 
 
-  console.log(citySearch);
+
+
+
+
+
+  //console.log(citySearch);
+
+
+
 
   //construct new request url- dynamic pull
 // make the fetch call
@@ -22,7 +61,7 @@ function getApi(event) {
   // traverse data 
   // display on page
 
-}
+
 
 
   //5 day weather fetch function.
