a60d09a26921f031f27e7f6633ee2d331ae46e6e
Shawn Paulsen <shawnpaulsen@Shawns-MacBook-Pro.local>
2021-04-12 10:30:57 -0600

    html skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,113 +1,203 @@
-let weatherContainer = document.getElementById('weather');
-let forecastContainer = document.getElementById('five-day');
-let historyContainer = document.getElementById('history');
-let searchCity = document.querySelector('#search-city');
-let searchButton = document.getElementById('search-button');
-let apiKey = '7b56d24fc2a9f800b5f359ee3bc2a2f5';
-//let cityArray = [];
-//searchBtn.addEventListener('click', getApi);
+var repoNameEl = document.querySelector('#repo-name');
+var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
 
-let formSubmitHandler = function (event) {
-  event.preventDefault();
+var getRepoName = function () {
+  // This is coming from the URL search bar in the browser. It is what comes after the `?`.
+  var queryString = document.location.search;
+  var repoName = queryString.split('=')[1];
+
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // This will run and return to the homepage if there was nothing in the URL query parameter.
+    document.location.replace('./index.html');
+  }
 };
 
+var getRepoIssues = function (repo) {
+  var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
 
-//loCALSTORAGE SET- set
-//localStorage get- load on page
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayIssues(data);
 
-
-function getApi(event) {
-  event.preventDefault();
-
-  //let searchValue = document.getElementById('search-city').value;
-  let searchValue = document.getElementById('search-city').value;
-
-  console.log(searchValue)
-  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}&units=imperial`;
-  
-  // repeat api fetch
-  fetch(weatherUrl)
-    .then(function (response) {
-      return response.json()
-    })
-
-    .then(function (data) {
-        console.log(data)
-        document.querySelector('#search-city').value = "";
-       
-        console.log('humidity:', data.main.humidity); 
-        console.log('weather:', data.main.temp); 
-        console.log('name:', data.name)
-      //Needs separate div
-      //document.querySelector('.results').textContent = data.main.humidity;
-        document.querySelector('.results').textContent = data.main.temp;
-
+        // Since GitHub only returns 30 results at a time, we check to see if there's more than 30 by looking for a next page URL in the response headers.
+        if (response.headers.get('Link')) {
+          displayWarning(repo);
+        }
       });
+    } else {
+      document.location.replace('./index.html');
+    }
+  });
+};
 
-      let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=${apiKey}&units=imperial`;
+var displayIssues = function (issues) {
+  // This will check for strict equality. Using `!issues.length` works, but only because JavaScript considers `0` to be `falsy`.
+  if (issues.length === 0) {
+    issueContainerEl.textContent = 'This repo has no open issues!';
+    return;
+  }
+
+  for (var i = 0; i < issues.length; i++) {
+    var issueEl = document.createElement('a');
+    issueEl.classList = 'list-item flex-row justify-space-between align-center';
+    issueEl.setAttribute('href', issues[i].html_url);
+    issueEl.setAttribute('target', '_blank');
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = issues[i].title;
+    issueEl.appendChild(titleEl);
+
+    var typeEl = document.createElement('span');
+
+    // If there's already a pull request open, it's a good idea we focus on other open issues that no one has worked on.
+    if (issues[i].pull_request) {
+      typeEl.textContent = '(Pull request)';
+    } else {
+      typeEl.textContent = '(Issue)';
+    }
+
+    issueEl.appendChild(typeEl);
+
+    issueContainerEl.appendChild(issueEl);
+  }
+};
+
+// When there are more issues than what GitHub has returned, we let the user know by printing a message with a link to the page.
+var displayWarning = function (repo) {
+  limitWarningEl.textContent = 'To see more than 30 issues, visit ';
+
+  var linkEl = document.createElement('a');
+  linkEl.textContent = 'GitHub.com';
+  linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+  linkEl.setAttribute('target', '_blank');
+
+  // This will appear on the bottom of the page.
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
+
+
+// //let cityArray = [];
+// //searchBtn.addEventListener('click', getApi);
+
+// let formSubmitHandler = function (event) {
+//   event.preventDefault();
+// };
+
+
+// //loCALSTORAGE SET- set
+// //localStorage get- load on page
+
+
+// function getApi(event) {
+//   event.preventDefault();
+
+//   //let searchValue = document.getElementById('search-city').value;
+//   let searchValue = document.getElementById('search-city').value;
+
+//   console.log(searchValue)
+//   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}&units=imperial`;
+  
+//   // repeat api fetch
+//   fetch(weatherUrl)
+//     .then(function (response) {
+//       return response.json()
+//     })
+
+//     .then(function (data) {
+//         console.log(data)
+//         document.querySelector('#search-city').value = "";
+        
+//         // let currentDate = document.createElement('div');
+//         // currentDate.textContent = " (" + moment(data.value).calendar("MMM D, YYYY") + ") ";
+
+//         // var weatherIcon= data.weather[0].icon;
+
+//         // weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+
+//         console.log('humidity:' + data.main.humidity + '%'); 
+//         console.log('weather:' + data.main.temp + 'F'); 
+//         console.log('name:', data.name)
+//       //Needs separate div
+      
+//         document.querySelector('.results').textContent = data.main.temp;
+
+//       });
+
+//       let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=${apiKey}&units=imperial`;
     
-      fetch(forecastUrl)
-      .then(function (response) {
-        return response.json()
-      })
+//       fetch(forecastUrl)
+//       .then(function (response) {
+//         return response.json()
+//       })
 
-      .then(function (data) {
-          console.log(data)
-          document.querySelector('#search-city').value = "";
-          // $('#search-city').val('')
-        });
+//       .then(function (data) {
+//           console.log(data)
+//           document.querySelector('#search-city').value = "";
+//           // $('#search-city').val('')
+//         });
 
-}
+// }
 
     
-  //Needs separate div
-  //document.querySelector('.results').textContent = data.main.humidity;
+//   //Needs separate div
+//   //document.querySelector('.results').textContent = data.main.humidity;
 
-  searchButton.addEventListener('click', getApi);
-
-
-  //data.weather[0].icon;
-  //let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=${apiKey}&units=imperial`;
-
-  //var = data.weather[0].icon;
-
-  // weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-
-//let cityname = searchCity.value();
-
-  // if (cityname) {
-  //   getCityNames (cityname) ;
-  //   searchCity.textContent = '';
-  //   nameInputEl.Value = '';
-  // } else {
-  //     alert('Please select a city.');
-  // }
+//   searchButton.addEventListener('click', getApi);
 
 
+//   //data.weather[0].icon;
 
-  // let temp = document.createElement('div');
-  // temp.textContent = "Temp: " + DataTransfer.main.temp + "F";
-  // temp.classList = "list-group";
+  
 
-  // let cityEl = document.createElement('h3');
-  // cityEl.textContent = data.name;
+//   //var = data.weather[0].icon;
 
-  // let humidity = document.createElement('div');
-  // humidity.textContent = "Humidity: " + data.main.humidity + "% ";
-  // humidity.classList = "list-group";
-
-
-
-  // let weatherIcon = document.createElement('img');
-  // weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-  // cityEl.appendChild(weatherIcon);
+//   // weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
 
 
 
 
-  // let currentDate = document.createElement('div');
-  // currentDate.textContent = " (" + moment(data.value).calendar("MMM D, YYYY") + ") ";
-  // cityEl.append(currentDate);
+// //let cityname = searchCity.value();
+
+//   //   getCityNames (cityname) ;
+//   //   searchCity.textContent = '';
+//   //   nameInputEl.Value = '';
+//   // } else {
+//   //     alert('Please select a city.');
+//   // }
+
+
+
+//   // let temp = document.createElement('div');
+//   // temp.textContent = "Temp: " + DataTransfer.main.temp + "F";
+//   // temp.classList = "list-group";
+
+//   // let cityEl = document.createElement('h3');
+//   // cityEl.textContent = data.name;
+
+//   // let humidity = document.createElement('div');
+//   // humidity.textContent = "Humidity: " + data.main.humidity + "% ";
+//   // humidity.classList = "list-group";
+
+
+
+//   // let weatherIcon = document.createElement('img');
+//   // weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+//   // cityEl.appendChild(weatherIcon);
+
+
+
+
+//   // let currentDate = document.createElement('div');
+//   // currentDate.textContent = " (" + moment(data.value).calendar("MMM D, YYYY") + ") ";
+//   // cityEl.append(currentDate);
 
 
 
@@ -115,10 +205,10 @@ function getApi(event) {
 
 
 
-  // function savedCities(searchedCity) {
-  //   cityArray.push(citySearch)
-  //   console.log('city array variable: ', cityArray);
-  //   localStorage.setItem('cities', JSON.stringify(cityArray));
+//   // function savedCities(searchedCity) {
+//   //   cityArray.push(citySearch)
+//   //   console.log('city array variable: ', cityArray);
+//   //   localStorage.setItem('cities', JSON.stringify(cityArray));
 
   // }
 
@@ -182,7 +272,4 @@ function getApi(event) {
   //           return response.json();
   //       })
   //       .then(function (data) {
-  //           console.log(data);
-  //       });
-
-  //     }        
+  //           console.log(data)
